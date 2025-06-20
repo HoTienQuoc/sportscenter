3676b5ce9edd687bee2d1b511da64c7840effeab
HoTienQuoc <hotienquoc0429@gmail.com>
2025-06-20 14:01:34 +0700

    new comit

--- src/main/java/com/ecomerce/sportscenter/config/WebConfig.java
+++ src/main/java/com/ecomerce/sportscenter/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.ecomerce.sportscenter.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
+@EnableSpringDataWebSupport
 public class WebConfig {
 
 }
