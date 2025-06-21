00900f8a4c2d37d4b4cf4daa53f816e5da11db5e
HoTienQuoc <hotienquoc0429@gmail.com>
2025-06-21 19:17:04 +0700

    new commit

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'client';
+  protected title = 'Sports Center';
 }
