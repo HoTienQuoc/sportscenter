ab1b0e936f6d30470d9751e290e51602a95ec2ef
HoTienQuoc <hotienquoc0429@gmail.com>
2025-06-21 21:22:13 +0700

    new commit

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavBar } from "./nav-bar/nav-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NavBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
