a4f122b2521d6e8824a1d659a87931f983d6a3ce
HoTienQuoc <hotienquoc0429@gmail.com>
2025-06-28 21:49:31 +0700

    new commit

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CoreModule } from './core/core-module';
 import { StoreModule } from './store/store-module';
 import { HomeModule } from './home/home-module';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CoreModule,StoreModule,HomeModule],
+  imports: [CoreModule,StoreModule,HomeModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
