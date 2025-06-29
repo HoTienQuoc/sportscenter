aa12fda0969d423f987b20650368beb947223d3e
HoTienQuoc <hotienquoc0429@gmail.com>
2025-06-30 05:01:41 +0700

    update CoreModule to include NotFound and ServerError components

--- client/src/app/core/core-module.ts
+++ client/src/app/core/core-module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBar } from './nav-bar/nav-bar';
 import { RouterModule } from '@angular/router';
+import { NotFound } from './not-found/not-found';
+import { ServerError } from './server-error/server-error';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,NavBar,
+    CommonModule,NavBar,NotFound,ServerError
   ],
-  exports: [NavBar]
+  exports: [NavBar,NotFound,ServerError]
 })
 export class CoreModule { }
