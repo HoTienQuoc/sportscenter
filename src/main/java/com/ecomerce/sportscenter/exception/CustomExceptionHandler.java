2228ec280df6c1b5651ca652ec48d1650743b599
HoTienQuoc <hotienquoc0429@gmail.com>
2025-06-30 19:48:53 +0700

    Update .DS_Store file to reflect changes in project structure

---
+++ src/main/java/com/ecomerce/sportscenter/exception/CustomExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.ecomerce.sportscenter.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler{
+
+}
---
+++ src/main/java/com/ecomerce/sportscenter/exception/ProductNotFoundException.java
@@ -0,0 +1,5 @@
+package com.ecomerce.sportscenter.exception;
+
+public class ProductNotFoundException {
+
+}
