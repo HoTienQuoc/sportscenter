4a5f1310ce0f0b3f9b52b4e48a53549713812e66
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-07 21:46:53 +0700

    Add authentication functionality in AuthController
    - Implemented login endpoint to authenticate users and generate JWT tokens.
    - Integrated UserDetailsService and AuthenticationManager for user validation.
    - Added error handling for invalid credentials during authentication.
    - Created JwtRequest and JwtResponse classes for request and response handling.

--- src/main/java/com/ecomerce/sportscenter/controller/AuthController.java
+++ src/main/java/com/ecomerce/sportscenter/controller/AuthController.java
@@ -1,5 +1,51 @@
 package com.ecomerce.sportscenter.controller;
 
-public class AuthController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ecomerce.sportscenter.response.JwtRequest;
+import com.ecomerce.sportscenter.response.JwtResponse;
+import com.ecomerce.sportscenter.security.JwtHelper;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationManager manager;
+    private final JwtHelper jwtHelper;
+    public AuthController(UserDetailsService userDetailsService, AuthenticationManager manager, JwtHelper jwtHelper) {
+        this.userDetailsService = userDetailsService;
+        this.manager = manager;
+        this.jwtHelper = jwtHelper;
+    }
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest request){
+        this.authenticate(request.getUsername(), request.getPassword());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        String token = this.jwtHelper.generateToken(userDetails);
+        JwtResponse response = JwtResponse.builder()
+                .username(userDetails.getUsername())
+                .token(token)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    private void authenticate(String username, String password) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        try{
+            manager.authenticate(authenticationToken);
+        }catch(BadCredentialsException e){
+            throw new BadCredentialsException("Invalid Username or Password");
+        }
+
+    }
 }
