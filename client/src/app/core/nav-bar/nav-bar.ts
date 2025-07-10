0d163e2be3a794f43ea88f059087280706546a84
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-10 17:10:50 +0700

    Enhance NavBar with user authentication links and styling improvements
    - Added conditional rendering in NavBar to display login and registration links for unauthenticated users, and a welcome message with logout option for authenticated users.
    - Updated NavBar styles for a fixed position at the top, improved link alignment, and added a cart badge for item count.
    - Refactored NavBar component to integrate AccountService for user state management and logout functionality.

--- client/src/app/core/nav-bar/nav-bar.ts
+++ client/src/app/core/nav-bar/nav-bar.ts
@@ -3,6 +3,9 @@ import { RouterLink, RouterModule } from '@angular/router';
 import { BasketItem } from '../../shared/models/basket';
 import { BasketService } from '../../basket/basket.service';
 import { AsyncPipe } from '@angular/common';
+import { AccountService } from '../../account/account.service';
+import { Observable } from 'rxjs';
+import { User } from '../../shared/models/user';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,9 +14,22 @@ import { AsyncPipe } from '@angular/common';
   styleUrl: './nav-bar.scss'
 })
 export class NavBar {
-  constructor(public basketService: BasketService){}
+  currentUser$?: Observable<User | null>;  
+
+  constructor(
+    public basketService: BasketService,    
+    public accountService: AccountService
+  ){}
+
+  ngOnInit(): void {
+    this.currentUser$ = this.accountService.currentUser$;  
+  }
 
   getItemsCount(items: BasketItem[]){
     return items.reduce((sum, item)=>sum+item.quantity, 0);
+  }
+
+  logout() {
+    this.accountService.logout();
   }
 }
