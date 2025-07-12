0fb9f956ead22ef81c1fbbef508102943ba79376
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-12 21:26:35 +0700

    Implement checkout review component with dynamic basket display
    - Added HTML template to display items in the basket, including product image, name, quantity, price, and total.
    - Enhanced ReviewComponent to manage basket data using BasketService and navigate to the store upon order submission.
    - Implemented image extraction logic for product display in the review section.

--- client/src/app/checkout/review/review.component.ts
+++ client/src/app/checkout/review/review.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { BasketService } from '../../basket/basket.service';
+import { Router } from '@angular/router';
+import { Basket, BasketItem } from '../../shared/models/basket';
 
 @Component({
   selector: 'app-review.component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './review.component.html',
   styleUrl: './review.component.scss'
 })
-export class ReviewComponent {
+export class ReviewComponent implements OnInit{
+  basket: Basket | null = new Basket();
+  
+  constructor(public basketService: BasketService, private router: Router){}
+  
+  ngOnInit(): void {
+    this.basketService.basketSource$.subscribe((basket) =>{
+      this.basket = basket;
+    })
+  }
 
+  extractImageName(item: BasketItem): string | null{
+    if(item && item.pictureUrl){
+      const parts = item.pictureUrl.split('/');
+      if(parts.length>0){
+        return parts[parts.length-1];
+      }
+    }
+    return null;
+  }
+  submitOrder(){
+    this.basketService.clearBasket();
+    this.router.navigate(['/store']);
+  }
 }
